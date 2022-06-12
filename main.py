41aea22a128edf99631df3d9eb6845499e1d3215
Lassana siby <103085452+sibylassana95@users.noreply.github.com>
2022-06-12 00:04:00 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+chaine1= input("Chaîne 1 : ")
+chaine2= input("Chaîne 2 : ")
+if ( len(chaine2) > len(chaine1) ) :
+  print (("Chaîne la plus grande : " + chaine2 ))
+else:
+ print (("Chaîne la plus grande : " + chaine1 ))
